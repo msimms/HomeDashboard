5a2bd9fc0b164d87006294e663d33acdb86782dd
Mike Simms <msimms01@gmail.com>
2024-06-03 20:50:43 -0400

    Initial code commit.

---
+++ src/arduino/weather_station/weather_station.ino
@@ -0,0 +1,41 @@
+void setup() {
+  Serial.begin(9600);
+  delay(1000); // Wait before accessing sensors
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void read_anemometer() {
+  float sensorValue = analogRead(A0);
+  Serial.print("Analog Value =");
+  Serial.println(sensorValue);
+ 
+  float voltage = (sensorValue / 1024) * 5;
+  Serial.print(voltage);
+  Serial.println(" V");
+ 
+  float wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
+  float speed_mph = ((wind_speed *3600)/1609.344);
+  Serial.print("Wind Speed =");
+  Serial.print(wind_speed);
+  Serial.println("m/s");
+  Serial.print(speed_mph);
+  Serial.println("mph");
+}
+
+void read_temperature() {
+
+}
+
+void post_to_web() {
+
+}
+
+void loop() {
+  read_anemometer();
+  read_temperature();
+  post_to_web();
+}
