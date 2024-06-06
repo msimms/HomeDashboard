71e5163f93e0d03ff6b7a8d279619c247dede637
Mike Simms <msimms01@gmail.com>
2024-06-06 16:21:13 -0400

    Can now read from the tempereature and humidity sensor.

--- src/arduino/weather_station/weather_station.ino
+++ src/arduino/weather_station/weather_station.ino
@@ -1,8 +1,11 @@
+#include <DHT11.h>
+
 #define DHT11_PIN 7
 
-dht DHT;
+DHT11 dht11(DHT11_PIN);
 
 void setup() {
+  Serial.println("Initializing...");
   Serial.begin(9600);
   delay(1000); // Wait before accessing sensors
 }
@@ -13,6 +16,8 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
 }
 
 void read_anemometer() {
+  Serial.println("Reading wind speed...");
+
   float sensorValue = analogRead(A0);
   Serial.print("Analog Value =");
   Serial.println(sensorValue);
@@ -31,19 +36,37 @@ void read_anemometer() {
 }
 
 void read_temperature() {
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+  Serial.println("Reading temperature and humidity...");
+
+  int temperature = 0;
+  int humidity = 0;
+
+  // Attempt to read the temperature and humidity values from the DHT11 sensor.
+  int result = dht11.readTemperatureHumidity(temperature, humidity);
+
+  // Check the results of the readings.
+  // If the reading is successful, print the temperature and humidity values.
+  // If there are errors, print the appropriate error messages.
+  if (result == 0) {
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.print(" °C\tHumidity: ");
+      Serial.print(humidity);
+      Serial.println(" %");
+  } else {
+      // Print error message based on the error code.
+      Serial.println(DHT11::getErrorString(result));
+  }
 }
 
 void post_to_web() {
+  Serial.println("Posting...");
 
 }
 
 void loop() {
-  read_anemometer();
+  Serial.println("Starting...");
+//  read_anemometer();
   read_temperature();
   post_to_web();
   delay(1000);
