22970a3c51cc4bc70b519b5392252bcc329ccb12
Mike Simms <msimms01@gmail.com>
2025-01-10 19:46:40 -0500

    Continuing to stub out the web app.

--- src/web/weather_station.py
+++ src/web/weather_station.py
@@ -1,21 +1,110 @@
 import argparse
 import flask
+import json
 import logging
+import os
 import signal
 import sys
+import traceback
+
+from mako.template import Template
 
 g_flask_app = flask.Flask(__name__)
+g_root_dir = ""
+g_root_url = ""
+g_tempmod_dir = "tempmod"
 
 ERROR_LOG = 'error.log'
+HTML_DIR = 'html'
 
 def signal_handler(signal, frame):
     print("Exiting...")
     sys.exit(0)
 
+def log_info(log_str):
+    """Writes an info message to the log file."""
+    logger = logging.getLogger()
+    logger.info(log_str)
+
+def log_error(log_str):
+    """Writes an error message to the log file."""
+    logger = logging.getLogger()
+    logger.error(log_str)
+
 @g_flask_app.route('/')
 def index():
     """Renders the index page."""
+    try:
+        html_file = os.path.join(g_root_dir, HTML_DIR, 'index.html')
+        my_template = Template(filename=html_file, module_directory=g_tempmod_dir)
+        return my_template.render(root_url=g_root_url)
+    except:
+        log_error("Unhandled Exception")
     return ""
+
+def handle_indoor_air_request(self):
+    return False, ""
+
+def handle_api_1_0_get_request(self, request, values):
+    """Called to parse a version 1.0 API GET request."""
+    if request == 'indoor_air':
+        return self.handle_indoor_air_request(values)
+    return False, ""
+
+def handle_api_1_0_post_request(self, request, values):
+    """Called to parse a version 1.0 API POST request."""
+    return False, ""
+
+def handle_api_1_0_delete_request(self, request, values):
+    """Called to parse a version 1.0 API DELETE request."""
+    return False, ""
+
+def handle_api_request(self, verb, request, values):
+    """Handles API requests."""
+    request = request.lower()
+    if verb == 'GET':
+        return self.handle_api_1_0_get_request(request, values)
+    if verb == 'POST':
+        return self.handle_api_1_0_post_request(request, values)
+    if verb == 'DELETE':
+        return self.handle_api_1_0_delete_request(request, values)
+    return False, ""
+
+@g_flask_app.route('/api/<version>/<method>', methods = ['GET','POST','DELETE'])
+def api(version, method):
+    """Endpoint for API calls."""
+    response = ""
+    code = 500
+    try:
+        # The the API params.
+        if flask.request.method == 'GET':
+            verb = "GET"
+            params = flask.request.args
+        elif flask.request.method == 'DELETE':
+            verb = "DELETE"
+            params = flask.request.args
+        elif flask.request.data:
+            verb = "POST"
+            params = json.loads(flask.request.data)
+        else:
+            verb = "GET"
+            params = ""
+
+        # Process the API request.
+        if version == '1.0':
+            handled, response = api_handler(verb, method, params)
+            if handled:
+                code = 200
+            else:
+                code = 400
+        else:
+            code = 400
+    except:
+        log_error(traceback.format_exc())
+        log_error(sys.exc_info()[0])
+        response = 'Unhandled exception in ' + api.__name__
+        log_error(response)
+    return response, code
 
 def main():
     global g_flask_app
@@ -31,7 +120,8 @@ def main():
     # Parse the command line options.
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, action="store", default="localhost", help="The host interface on which to bind.", required=False)
-    parser.add_argument("--port", type=int, action="store", default=5000, help="The host port on which to bind.", required=False)
+    parser.add_argument("--port", type=int, action="store", default=5050, help="The host port on which to bind.", required=False)
+    parser.add_argument("--database", type=str, action="store", default="mongodb://localhost:27017", help="The URI for connecting to the database.", required=False)
 
     try:
         args = parser.parse_args()
@@ -47,7 +137,7 @@ def main():
         args.host = "localhost"
 
     # Create the app object. It contains all the functionality.
-    print(f"The app is running on {args.host}:{args.port}")
+    print(f"The app is running on http://{args.host}:{args.port}")
     g_flask_app.run(host=args.host, port=args.port)
 
 if __name__=="__main__":
