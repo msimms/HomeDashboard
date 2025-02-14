abfba211cd5df494447fc9427878c155884b75f7
Mike Simms <msimms01@gmail.com>
2025-02-14 16:52:26 -0500

    Wind speed and JSON formatting.

--- src/outdoors/weather_station/weather_station.ino
+++ src/outdoors/weather_station/weather_station.ino
@@ -4,12 +4,19 @@
 #include <WiFiNINA.h>
 #include "arduino_secrets.h" 
 
-#define MIN_VOLTAGE 0.4
+#define REF_VOLTAGE 3.3
+#define ADC_RESOLUTION 4095
+
+// Anemometer (https://www.adafruit.com/product/1733)
+#define MIN_ANEMOMETER_VOLTAGE 0.4
+#define MAX_ANEMOMETER_VOLTAGE 2.0
+#define MIN_WIND_SPEED 0.2  // meters per sec
+#define MAX_WIND_SPEED 32.4 // meters per sec
 
 // Wifi
-char ssid[] = SECRET_SSID;            // your network SSID (name)
-char pass[] = SECRET_PASS;            // your network password (use for WPA, or use as key for WEP)
-int wifiStatus = WL_IDLE_STATUS;      // the Wi-Fi radio's status
+char ssid[] = SECRET_SSID;       // your network SSID (name)
+char pass[] = SECRET_PASS;       // your network password (use for WPA, or use as key for WEP)
+int wifi_status = WL_IDLE_STATUS; // the Wi-Fi radio's status
 
 // The "pin" for the onboard LED.
 int LED = 13;
@@ -28,43 +35,22 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
 }
 
 /// @function read_anemometer
-void read_anemometer() {
-  Serial.println("Reading wind speed...");
+float read_anemometer() {
+  float sensor_value = analogRead(A0);
+  float voltage = (sensor_value * REF_VOLTAGE) / ADC_RESOLUTION;
+  float wind_speed_ms = 0.0;
 
-  float sensorValue = analogRead(A0);
-  Serial.print("Analog Value: ");
-  Serial.println(sensorValue);
-
-  float voltage = (sensorValue / 1024) * 5;
-  Serial.print(voltage);
-  Serial.println(" V");
-
-  float wind_speed = 0.0;
-  if (voltage >= MIN_VOLTAGE) {
-    wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
+  if (voltage >= MIN_ANEMOMETER_VOLTAGE) {
+    wind_speed_ms = mapfloat(voltage, MIN_ANEMOMETER_VOLTAGE, MAX_ANEMOMETER_VOLTAGE, MIN_WIND_SPEED, MAX_WIND_SPEED);
   }
-
-  float speed_mph = ((wind_speed * 3600) / 1609.344);
-  Serial.print("Wind Speed: ");
-  Serial.print(wind_speed);
-  Serial.print(" m/s (");
-  Serial.print(speed_mph);
-  Serial.println(" mph)");
+  return wind_speed_ms;
 }
 
 /// @function read_temperature_and_humidity_from_am2315
-void read_temperature_and_humidity_from_am2315c() {
-  Serial.println("Reading temperature and humidity...");
-
+void read_temperature_and_humidity_from_am2315c(float* temp_c, float* humidity) {
   int status = DHT.read();
-  float temperature = DHT.getTemperature();
-  float humidity = DHT.getHumidity();
-
-  // Format the output.
-  char buff[32];
-  snprintf(buff, sizeof(buff) - 1, "%f\t%f", temperature, humidity);
-
-  Serial.println(buff);
+  *temp_c = DHT.getTemperature();
+  *humidity = DHT.getHumidity();
 }
 
 /// @function setup_wifi
@@ -74,11 +60,11 @@ void setup_wifi() {
   // Set the LED as output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // attempt to connect to Wi-Fi network:
-  while (wifiStatus != WL_CONNECTED) {
+  // Attempt to connect to Wi-Fi network:
+  while (wifi_status != WL_CONNECTED) {
     Serial.print("Attempting to connect to network: ");
     Serial.println(ssid);
-    wifiStatus = WiFi.begin(ssid, pass);
+    wifi_status = WiFi.begin(ssid, pass);
 
     // Wait 10 seconds for connection.
     delay(10000);
@@ -134,7 +120,7 @@ void setup() {
   }
 
   // Initialize the anemometer.
-  //setup_anemometer();
+  setup_anemometer();
 
   // Initialize the AM2315.
   setup_am2315();
@@ -145,11 +131,19 @@ void loop() {
   // Turn the LED on.
   digitalWrite(LED, HIGH);
 
-  //read_anemometer();
-  read_temperature_and_humidity_from_am2315c();
+  float wind_speed_ms = read_anemometer();
+  float temp_c = 0.0;
+  float humidity = 0.0;
+  read_temperature_and_humidity_from_am2315c(&temp_c, &humidity);
 
   // Turn the LED off.
   digitalWrite(LED, LOW);
+
+  // Format the output.
+  char buff[128];
+  snprintf(buff, sizeof(buff) - 1, "{\"wind speed ms\": %f, \"temperature\": %f, \"humidity\": %f}", wind_speed_ms, temp_c, humidity);
+
+  Serial.println(buff);
 
   delay(10000);
 }
