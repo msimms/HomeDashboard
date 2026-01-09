eb5acd80a5f20e2c7b3e35591ec106dbb71c7b9a
Mike Simms <msimms01@gmail.com>
2026-01-09 17:02:12 -0500

    Air quality sensor dislay updates.

--- src/indoors/air_quality_sensor/air_quality_sensor.ino
+++ src/indoors/air_quality_sensor/air_quality_sensor.ino
@@ -20,21 +20,24 @@
 #define SCREEN_ADDRESS 0x3C // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// The devices.
 SensirionI2cScd4x scd4x; // CO2, temp, and humidity
 Adafruit_SGP40 sgp; // VOC
 
 // The "pin" for the onboard LED.
 int LED = 13;
 
-/// @function updateDisplay
-void updateDisplay(char* msg) {
+// For the display.
+#define LEFT_TEXT_MARGIN 4
+#define TOP_TEXT_MARGIN 12
+
+/// @function update_display
+void update_display(char* line) {
   display.clearDisplay();
-
-  display.setTextSize(1); // Normal 1:1 pixel scale
+  display.setTextSize(2); // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0,0); // Start at top-left corner
-  display.println(msg);
-
+  display.setCursor(LEFT_TEXT_MARGIN, TOP_TEXT_MARGIN);
+  display.println(line);
   display.display();
 }
 
@@ -56,7 +59,7 @@ void setup() {
     display.display();
   }
   else {
-    Serial.println(F("Error: SSD1306 allocation failed!"));
+    Serial.println("Error: SSD1306 allocation failed!");
   }
 
   // Start the CO2 sensor.
@@ -65,7 +68,7 @@ void setup() {
   // Ensure sensor is in clean state
   uint16_t error = scd4x.wakeUp();
   if (error) {
-      Serial.println("Error trying to execute wakeUp()!");
+    Serial.println("Error trying to execute wakeUp()!");
   }
 
   // Stop potentially previously started measurement.
@@ -82,7 +85,7 @@ void setup() {
 
   // Start the SGP VOC sensor.
   if (!sgp.begin()){
-    Serial.println("SGP40 sensor not found :(");
+    Serial.println("Error: SGP40 sensor not found!");
   }
 
   // Force a short wait before attempting the first read
@@ -92,6 +95,7 @@ void setup() {
 /// @function loop
 /// Called repeatedly
 void loop() {
+
   // Turn the LED on.
   digitalWrite(LED, HIGH);
 
@@ -100,33 +104,61 @@ void loop() {
   float temperature;
   float humidity;
   uint16_t error = scd4x.readMeasurement(co2, temperature, humidity);
+
+  // Turn the LED off.
+  digitalWrite(LED, LOW);
+
+  // Display the results.
   if (error) {
     Serial.println("Error: Failed to execute readMeasurement()!");
   } else if (co2 == 0) {
     Serial.println("Error: Invalid sample detected, skipping.");
   } else {
+
     // Read the raw VOC measurement as well as the temperature and humidity compensated version.
     uint16_t sraw = sgp.measureRaw(temperature, humidity);
     int32_t voc_index = sgp.measureVocIndex(temperature, humidity);
 
     // Format the output.
-    char tempBuf[12];
-    dtostrf(temperature, 4, 2, tempBuf);
-    char humidityBuf[12];
-    dtostrf(humidity, 4, 2, humidityBuf);
+    char temp_buf[12];
+    dtostrf(temperature, 4, 2, temp_buf);
+    char humidity_buf[12];
+    dtostrf(humidity, 4, 2, humidity_buf);
     char buff[32];
-    snprintf(buff, sizeof(buff) - 1, "%u\t%s\t%s\t%u\t%u\t", co2, tempBuf, humidityBuf, sraw, voc_index);
+    snprintf(buff, sizeof(buff) - 1, "%u\t%s\t%s\t%u\t%u\t", co2, temp_buf, humidity_buf, sraw, voc_index);
 
-    // Output to serial:
+    // Output to serial.
     Serial.println(buff);
 
-    // Output to the display:
-    updateDisplay(buff);
+    // Reformat the output for the attached display.
+    // Present as a carousel as the screen is small and hard to read.
+    // This will also implement the delay until the next read.
+    for (int i = 0; i < 5; ++i) {
+      char line[32];
+
+      switch (i) {
+        case 0:
+          snprintf(line, sizeof(line) - 1, "%u ppm", co2);
+          update_display(line);
+          break;
+        case 1:
+          snprintf(line, sizeof(line) - 1, "%s C", temp_buf);
+          update_display(line);
+          break;
+        case 2:
+          snprintf(line, sizeof(line) - 1, "%s %%", humidity_buf);
+          update_display(line);
+          break;
+        case 3:
+          snprintf(line, sizeof(line) - 1, "VOC %u", sraw);
+          update_display(line);
+          break;
+        case 4:
+          snprintf(line, sizeof(line) - 1, "VOCI %u", voc_index);
+          update_display(line);
+          break;
+      }
+      delay(12000);
+    }
   }
-
-  // Turn the LED off.
-  digitalWrite(LED, LOW);
-
-  // Rate limit.
-  delay(600000);
 }
